9d9c9d417bf44033bf8573dbd9c7a3d9c6a99ee9
Walter Hoerman <whoerman@gmail.com>
2020-02-29 08:10:44 -0500

    adding collaborator fork

--- index.js
+++ index.js
@@ -2,37 +2,80 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 const fs = require('fs');
 
-inquirer
-  .prompt([
-    {
-      type: "confirm",
-      message: `
+
+
+function checkName() {
+  return inquirer.prompt([{
+        type: "confirm",
+        message: `
       Welcome to readMe Generator!
       Let me ask you a series of questions in order to generate
       a good readMe for your GitHub repository.
 
-      enter y to begin...
+      enter Y to begin...
       
       `,
-      name: "yesStart"
-    },
-    {
-      type: "input",
-      message: `
+        name: "yesStart"
+      },
+      {
+        type: "input",
+        message: `
       What is your GitHub user name?
       (we are going to get some information from your public profile)
       `,
-      name: "username"
-    },
-    {
-      type: "input",
-      message: `
-      We will try to get your email from your GitHub, 
-      but if the privacy settings do not allow access, what is your email?
-      `,
-      name: "backupEmail"
-    },
-    {
+        name: "username"
+      }
+    ])
+    .then(function (nameRes) {
+      let username = nameRes.username;
+      async function getGitHub() {
+
+        let result = await axios.get(`https://api.github.com/users/${username}`);
+        let userGitName = result.data.name;
+        let userCompany = result.data.company;
+        let location = result.data.location;
+        let userPicURL = result.data.avatar_url;
+        let userBio = result.data.bio;
+        let gitInfo = {
+          "Name": userGitName,
+          "Location": location,
+          "Picture": userPicURL,
+          "Company": userCompany,
+          "Bio": userBio
+        }
+
+        let userEmail = result.data.email;
+        if (userEmail === null) {
+          return inquirer.prompt([{
+            type: "input",
+            message: `
+            Due to privacy settings, you email was not available from GitHub.
+            So we can include it on your readMe, what is your email?
+            `,
+            name: "userEmail"
+          }]).then(function (backUpResult) {
+            return secondQuestions({
+              ...gitInfo,
+              ...nameRes,
+              ...backUpResult
+            });
+          })
+        } else {
+          return secondQuestions(nameRes);
+        }
+      }
+
+      try {
+        return getGitHub();
+      } catch (err) {
+        console.log("error")
+      }
+
+    })
+}
+
+function secondQuestions(prevResults) {
+  return inquirer.prompt([{
       type: "input",
       message: `
       What is your position on this project? (developer, coder, etc)
@@ -42,69 +85,75 @@ inquirer
     {
       type: "input",
       message: `
-      What is the Title of your Project?
-      `,
+    What is the Title of your Project?
+    `,
       name: "projectTitle"
     },
     {
       type: "input",
       message: `
-      Please describe your project in a short sentence
-      `,
+    Please describe your project in a short sentence:
+    `,
       name: "projectDescription"
     },
     {
       type: "input",
       message: `
-      Why did you want to develop this project?
-      `,
+    Why did you want to develop this project?
+    `,
       name: "projectBecause"
     },
     {
       type: "input",
       message: `
-      What are the steps required to install your project? 
-      Provide a step-by-step description of how to get the development environment running.
-      `,
+    What are the steps required to install your project? 
+    Provide a step-by-step description of how to get the development environment running.
+    `,
       name: "projectInstallation"
     },
-    {
-      type: "confirm",
-      message: `
-      Did you have any collaborators on this project? (y/n)
-      `,
-      name: "projectInstallation"
-    },
-    {
-      type: "input",
-      name: 'collab',
-      message: `
-      What was their names?
-      `
-      },
-  ])
-  .then(function (inquirerRes) {
-    let username = inquirerRes.username;
-    let projTitle = inquirerRes.projectTitle;
-    let projDesc = inquirerRes.projectDescription;
-    async function getGitHub() {
-
-      let result = await axios.get(`https://api.github.com/users/${username}`);
-
-      let userGitName = result.data.name;
-      let userEmail = result.data.email;
-      if (userEmail === null) {
-        userEmail = inquirerRes.backupEmail;
-      }
-      let location = result.data.location;
-
-      console.log(`Name: ${userGitName}`);
-      console.log(`Email: ${userEmail}`);
-      console.log(`Location: ${location}`);
-      console.log(`Your Project Tile: ${projTitle}`);
-      console.log(`Summary: ${projDesc}`);
-      console.log(result.data.avatar_url);
-      
+  ]).then(function (newResults) {
+    let combineResults = {
+      ...prevResults,
+      ...newResults
     };
-    getGitHub();
+    return Promise.resolve(combineResults);
+  });
+};
+
+checkName().then(function (combineResults) {
+  checkCol(combineResults);
+})
+
+function checkCol(combineResults) {
+  return inquirer.prompt([{
+    type: "confirm",
+    message: `
+  Did you have any collaborators on this project?
+  (y/n) 
+  `,
+    name: "yesCol"
+  }]).then(function (colQ) {
+    let combineResults2 = {
+      ...combineResults,
+      ...colQ
+    };
+    console.log(combineResults2);
+    if (colQ.yesCol) {
+      addCollab(combineResults2);
+    } else {
+      thirdQuestions(combineResults2);
+    }
   })
+}
+
+function addCollab(combineResults3) {
+  console.log("abby helped");
+  let combineResults4 = combineResults3;
+  console.log(combineResults4);
+}
+
+function thirdQuestions(combineResults3) {
+  console.log("no one helped");
+  let combineResults4 = combineResults3;
+  console.log(combineResults4);
+}
